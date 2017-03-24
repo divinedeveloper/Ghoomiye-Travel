99a4d6d52470567a67b8fa7ccbc57715fec4eda1
Swapnil Sawant <sawant.swapnil1@gmail.com>
2017-03-24 15:17:21 +0530

    ghoomiye- travel-search application in play framework

---
+++ app/models/Property.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.io.Serializable;
+
+import play.db.ebean.Model;
+
+public class Property extends Model implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
